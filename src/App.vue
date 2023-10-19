6bfdb45ba0b2107ce03e7028b2c11ee16cf0792d
Joao Vitor Ribeiro <joaovitordias.2b@gmail.com>
2023-10-18 22:05:38 -0300

    Exercicio feito

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<script >
+  export default{
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao:'soma',
+    }
+  },
+  computed: {
+    resultado() {
+      switch(this.operacao){
+        case 'soma':
+          return this.numero1 + this.numero2;
+        case 'subtracao':
+          return this.numero1 - this.numero2;  
+        case 'multiplicacao':
+          return this.numero1 * this.numero2; 
+        case 'divisao':
+          return this.numero2 !== 0 ? this.numero1 / this.numero2 : 'infinito';
+          default:
+          return 0;   
+      }
+    },
+  },
 }
 </script>
 
+<template>
+  <div id="container">
+      <h1>Calculadora Vue</h1>
+  <div>
+    <label for="numero1">Primeiro número:</label>
+    <input v-model.number="numero1" @input="calcular">
+  </div>
+  <div class="operacao">
+    <label for="operacao">Operações:</label>
+    <select v-model="operacao" @change="calcular">
+      <option value="soma">+</option>
+      <option value="subtracao">-</option>
+      <option value="multiplicacao">*</option>
+      <option value="divisao">/</option>
+    </select>
+  </div>
+  <div>
+    <label for="numero2">Segundo número:</label>
+    <input v-model.number="numero2" @input="calcular">
+  </div>
+  <div>
+    <h2>Resultado: {{ resultado }}</h2>
+  </div>
+</div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #container{
+    font-family: Arial, sans-serif;
+    text-align: center;
+    padding-bottom: 20px;
+    color: #2c3e50;
+  }
+  .operacao{
+    margin: 20px;
+  }
 </style>
